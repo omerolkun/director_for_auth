4ea5f755b6f3c4e874faf31be1dacfc6108a216c
omerolkun <omer.olkun@ug.bilkent.edu.tr>
2021-08-11 02:48:26 +0300

    edit director is not finihed

--- app_director/urls.py
+++ app_director/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index_page'),
     path('add_director', views.addDirector,name='add_director_page'),
+    path('detail_director/<int:idid>',views.detail,name='detail_page'),
+    path('edit/',views.edit,name='edit_page'),
 ]
--- app_director/views.py
+++ app_director/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import RequestAborted
 from django.shortcuts import render
 from .models import Director
 from .forms import YonetmenForm
@@ -17,8 +18,29 @@ def index(request):
     return render (request, 'app_d/index.html',context)
 
 def addDirector(request):
-    form = YonetmenForm()
+    if request.method == "POST":
+        form = YonetmenForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return index(request)
+    else:
+            form = YonetmenForm()
     context = {
-        'q':form,
+        'form':form,
     }
     return render(request,'app_d/add_director.html',context)
+
+
+def detail(request,idid):
+    context = {
+        'q':idid,
+    }
+    return render(request, 'app_d/detail.html',context)
+
+
+def edit(request):
+    form = YonetmenForm()
+    context = {
+        'form':form,
+    }
+    return render (request,'app_d/edit_director.html',context)
